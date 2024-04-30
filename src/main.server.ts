884f4be3154e91e6d8b99af4d122c7c541c42776
haruki-dayooo <ducluongdn3@gmail.com>
2024-04-30 22:28:58 +0700

    Section 1 lecture 6

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { HomeComponent } from './app/home/home.component';
 import { config } from './app/app.config.server';
+import { OrderComponent } from './app/order/order.component';
+import { OrderConfirmComponent } from './app/order-confirm/order-confirm.component';
 
-const bootstrap = () => bootstrapApplication(HomeComponent, config);
+const bootstrap = () => bootstrapApplication(OrderConfirmComponent, config);
 
 export default bootstrap;
